c1b7f1f1527c6648165cfec2736dc6cd29fef365
AmyCheng <amycheng@sugarfun.com.tw>
2021-07-21 16:44:04 +0800

    Handle full website layout

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,6 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+const blogBtn = document.getElementsByClassName('jBlogBtn');
+for(let i=0; i<blogBtn.length; i++){
+  blogBtn[i].addEventListener('click', () => {
+    location.href="post.html";
+  });
+}
